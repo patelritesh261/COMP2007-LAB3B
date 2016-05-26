51888d34b34f19a85bcd9b1275bf93c71afdfe53
patel <patel@RITZ>
2016-05-26 14:11:16 -0400

    added user-control

--- COMP2007-LAB3B/Contact.aspx
+++ COMP2007-LAB3B/Contact.aspx
@@ -1,4 +1,4 @@
-﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="Contact.aspx.cs" Inherits="COMP2007_LAB3B.Contact" %>
+﻿<%@ Page Title="Contact Page" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="Contact.aspx.cs" Inherits="COMP2007_LAB3B.Contact" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
---
+++ COMP2007-LAB3B/User_Controls/Navbar.ascx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace COMP2007_LAB3B
+{
+    public partial class Navbar : System.Web.UI.UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            SetActivePage();
+           
+        }
+        /*
+         * This method adds css class of active to navigation 
+         * link to each page
+         * 
+         * @method SetActivePage
+         * @return {void}
+         * */
+        private void SetActivePage()
+        {
+            switch (Page.Title)
+            {
+                case "Home Page":
+                    home.Attributes.Add("class", "active");
+                    break;
+                case "Contact Page":
+                    contact.Attributes.Add("class", "active");
+                    break;
+            }
+        }
+    }
+}
